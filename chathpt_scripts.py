0bd169fed5f8abfdf2d176dfb45d5ca4c7fa35ca
zhang tao <zhang_tao@whu.edu.cn>
2023-12-29 11:40:39 +0800

    update

--- chathpt_scripts.py
+++ chathpt_scripts.py
@@ -6,21 +6,37 @@ from langchain.schema import (
     SystemMessage
 )
 my_keys = 'sk-hlV40Lxa6Ce1OwJg89083e' + '8b005040D2A10eFa2c096631Bd'
-openai.api_key = my_keys
+# openai.api_key = my_keys
 
-# system prompt，用于告诉GPT当前的情景，不了解可以放空，没有影响。
-# system prompt例如：'You are a marketing consultant, please answer the client's questions in profession style.'
-system_content = ''
+# # system prompt，用于告诉GPT当前的情景，不了解可以放空，没有影响。
+# # system prompt例如：'You are a marketing consultant, please answer the client's questions in profession style.'
+# system_content = ''
+#
+# # 这里使用了langchain包简化与GPT的对话过程，基于的是GPT-3.5，能力与免费版的chatGPT相同。GPT-4需要自行申请加入waitlist
+# #messages = [SystemMessage(content=system_content)]
+#
+# def chat(questions):
+#     chat = ChatOpenAI(temperature=0.2, openai_api_key=openai.api_key)
+#     messages = [SystemMessage(content=system_content)]
+#     messages.append(HumanMessage(content=questions))
+#     response = chat(messages)
+#     return response.content
+#
+# ret = chat("please transfer this text into chinese: Can use the slides you prepared before ha.")
+# print(ret)
 
-# 这里使用了langchain包简化与GPT的对话过程，基于的是GPT-3.5，能力与免费版的chatGPT相同。GPT-4需要自行申请加入waitlist
-#messages = [SystemMessage(content=system_content)]
 
-def chat(questions):
-    chat = ChatOpenAI(temperature=0.2, openai_api_key=openai.api_key)
-    messages = [SystemMessage(content=system_content)]
-    messages.append(HumanMessage(content=questions))
-    response = chat(messages)
-    return response.content
+from openai import OpenAI
 
-ret = chat("please transfer this text into chinese: Can use the slides you prepared before ha.")
-print(ret)
+api_base = "https://cd.aiskt.com/v1"
+client = OpenAI(api_key=my_keys, base_url=api_base)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
