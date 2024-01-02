6a01eb75b6c02a9501e8780d8002d5c072d8b118
zhang tao <zhang_tao@whu.edu.cn>
2024-01-02 16:15:13 +0800

    update

--- chatgpt_scripts/motion_summerize.py
+++ chatgpt_scripts/motion_summerize.py
@@ -1,6 +1,6 @@
 my_keys = 'sk-hlV40Lxa6Ce1OwJg89083e' + '8b005040D2A10eFa2c096631Bd'
 
-format_captions = 'Locations: {frame1: [0.786, 0.44, 0.848, 0.69], frame2: [0.756, 0.282, 0.821, 0.534]}'
+format_captions = 'Locations: {frame1: [0.786, 0.44, 0.848, 0.69], frame2: [0.706, 0.282, 0.771, 0.534]}'
 system_messages = "You are an AI visual assistant. Given the position of an object in consecutive video frames, please summarize its direction of motion. The position of each object is provided in the form of an outer bounding box, {x1, y1, x2, y2}, where x1, y1 represent the position of the top-left corner of the box, and x2, y2 represent the position of the bottom-right corner of the box. The positions of an object between two frames follows the following format: {frame1: [x1, y1, x2, y2], frame2: [x1, y1, x2, y2]}. x refers to the distance from the left side of the image, and y refers to the distance from the top of the image.\n \
 There are some examples:\n \
 Locations: {frame1: [0.261, 0.101, 0.787, 0.626], frame2: [0.152, 0.099, 0.676, 0.624]}\n \
